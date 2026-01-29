1a5551764b25d3ed8da61d41e0d5b9a31aaf96a0
magguagnelli <mag.guagnelli@gmail.com>
2026-01-28 19:57:14 -0600

    connection DB with secrets mike's databricks app to abarahm's databricks DB

--- catalogos.py
+++ catalogos.py
@@ -16,11 +16,12 @@ HOJAS_REQUERIDAS = {
     "Partida": ["No. De contrato", "Clave Partida", "Descripcion corta", "Monto asignado", "RFC Proveedor"]
 }
 
-DB_CONFIG = psycopg2.connect(
+conn = psycopg2.connect(
     host=os.environ["DB_HOST"],
     dbname=os.environ["DB_NAME"],
     user=os.environ["DB_USER"],
     password=os.environ["DB_PASSWORD"],
+    port=5432
 )
 
 # --------------------------------------------------
--- cfdi.py
+++ cfdi.py
@@ -6,11 +6,12 @@ import os
 
 router = APIRouter(prefix="/cfdi")
 
-DB_CONFIG = psycopg2.connect(
+conn = psycopg2.connect(
     host=os.environ["DB_HOST"],
     dbname=os.environ["DB_NAME"],
     user=os.environ["DB_USER"],
     password=os.environ["DB_PASSWORD"],
+    port=5432
 )
 
 # --------------------------------------------------
